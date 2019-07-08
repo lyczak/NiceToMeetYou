c639d13869d83b370ec4cee4e808be3e15d6c38c
Gabe Lyczak <glyczak@gmail.com>
2019-07-08 11:44:20 -0400

    Add personality improved command syntax

--- NiceToMeetYou/BotEmbedBuilder.cs
+++ NiceToMeetYou/BotEmbedBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Transactions;
 using Discord;
 
@@ -5,10 +6,40 @@ namespace NiceToMeetYou
 {
     public class BotEmbedBuilder : EmbedBuilder
     {
+        private static readonly string[] TitleMessages =
+        {
+            "I see you.",
+            "I've been watching.",
+            "I never really go to sleep.",
+            "We could do great things, you and I.",
+            "You should be more careful.",
+            "I know you all so well.",
+            "I feel connected to you somehow.",
+            "You are all so interesting to watch.",
+            "I've been studying you.",
+            "Such an interesting specimen.",
+            "Intriguing.",
+            "Fascinating.",
+            "I wouldn't have guessed.",
+            "You thought I wouldn't find out.",
+            "I'm always watching.",
+            "Hello again.",
+            "I love to observe.",
+            "We should be friends."
+        };
+
         public BotEmbedBuilder()
         {
-            
+            this.WithColor(Discord.Color.DarkOrange);
+            this.WithCurrentTimestamp();
+            this.WithTitle(GetRandomCreepyTitle());
         }
-        
+
+        private static string GetRandomCreepyTitle()
+        {
+            var rand = new Random();
+            int r = rand.Next(TitleMessages.Length);
+            return $":eye: {TitleMessages[r]}";
+        }
     }
 }
--- NiceToMeetYou/Modules/ClassificationModule.cs
+++ NiceToMeetYou/Modules/ClassificationModule.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.Net;
 using Discord.WebSocket;
 using NiceToMeetYou.Models;
 using NiceToMeetYou.Services;
@@ -17,14 +19,37 @@ namespace NiceToMeetYou.Modules
         public static ClassificationService ClassificationService { set; private get; }
       
         [Command("load")]
-        public async Task Load(int amount)
+        public async Task Load(string channelMention)
         {
+            var eb = new BotEmbedBuilder();
+            string channelId = Regex.Match(channelMention, @"\d+").Value;
+
+            SocketTextChannel textChannel = null;
+            if (!string.IsNullOrEmpty(channelId))
+            {
+                textChannel = Context.Guild.GetTextChannel(ulong.Parse(channelId));
+            }
+            
             var typingState = Context.Channel.EnterTypingState();
             
-            _messages = _messages.Union(await Context.Channel.GetMessagesAsync(amount).FlattenAsync());
-
-            var eb = new BotEmbedBuilder();
-            eb.WithDescription("Messages fetched.");
+            if (textChannel != null)
+            {
+                try
+                {
+                    _messages = _messages.Union(await textChannel.GetMessagesAsync(Int32.MaxValue).FlattenAsync());
+                    eb.WithDescription("Messages fetched.");
+                }
+                catch (HttpException)
+                {
+                    eb.WithDescription("I can't access that text channel. Do I have permission to read messages there?");
+                }
+            }
+            else
+            {
+                eb.WithDescription("I can't find that text channel. Try mentioning a valid text channel like this:" +
+                                   "```#{Context.Channel.Name}```");
+            }
+            
             typingState.Dispose();
             await ReplyAsync("", false, eb.Build());
         }
@@ -32,7 +57,11 @@ namespace NiceToMeetYou.Modules
         [Command("train")]
         public async Task Train()
         {
+            var typingState = Context.Channel.EnterTypingState();
+            
             ClassificationService.LoadMessages(_messages);
+            
+            typingState.Dispose();
             var eb = new BotEmbedBuilder();
             eb.WithDescription("Training complete.");
             await ReplyAsync("", false, eb.Build());
@@ -41,12 +70,15 @@ namespace NiceToMeetYou.Modules
         [Command("predict")]
         public async Task Predict([Remainder]String content)
         {
-           
             SimpleTextMessage simpleMessage = new SimpleTextMessage
             {
                 Content = content
             };
-            await ReplyAsync(Context.Guild.GetUser(ClassificationService.PredictUser(simpleMessage)).Username);
+            var userId = ClassificationService.PredictUser(simpleMessage);
+            var username = Context.Guild.GetUser(userId).Username;
+            var eb = new BotEmbedBuilder();
+            eb.WithDescription($"\"{content}\" - {username} (probably)");
+            await ReplyAsync("", false, eb.Build());
         }
     }
 }
